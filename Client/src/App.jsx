ef2b20589f93fa7ec3a67e04fb77fe2801542b97
Valorieb <valoriebroderick@gmail.com>
2024-01-16 18:26:54 -0500

    updates axios endpoint

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -10,7 +10,7 @@ import { Toaster } from "react-hot-toast";
 import Dashboard from "./pages/Dashboard";
 import { UserContextProvider } from "./context/userContext";
 
-axios.defaults.baseURL = "https://bad-bank-mern-valories-projects.vercel.app/";
+axios.defaults.baseURL = "https://mern-bb-valories-projects.vercel.app/";
 axios.defaults.withCredentials = true;
 
 function App() {
