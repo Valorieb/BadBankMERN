3789df9516bf14c4a6863d41f8f42ca25402573c
Valorieb <valoriebroderick@gmail.com>
2024-01-16 17:01:40 -0500

    fixes typos

--- server/index.js
+++ server/index.js
@@ -28,9 +28,9 @@ app.use(
 app.use("/", require("./routes/authRoutes"));
 
 // Serve static files from the React app
-app.use(express.static(path.join(__dirname, "client", "build")));
+app.use(express.static(path.join(__dirname, "Client", "build")));
 
 // Handle other routes and return the React app
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+  res.sendFile(path.join(__dirname, "Client", "build", "index.html"));
 });
